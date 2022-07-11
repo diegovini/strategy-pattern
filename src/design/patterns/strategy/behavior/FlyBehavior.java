db6f8e0a8d570d22e700346d61ecaa978d5d173d
diegovini <109113877+diegovini@users.noreply.github.com>
2022-07-11 20:35:05 -0300

    Delete src directory

--- src/design/patterns/strategy/behavior/FlyBehavior.java
+++
@@ -1,6 +0,0 @@
-package design.patterns.strategy.behavior;
-
-public interface FlyBehavior {
-	public void fly();
-
-}
--- src/design/patterns/strategy/behavior/QuackBehavior.java
+++
@@ -1,6 +0,0 @@
-package design.patterns.strategy.behavior;
-
-public interface QuackBehavior {
-	public void quack();
-
-}
